810ee253bc19f483d7e5d1eb968acbcc625334ae
sinyoung0403 <tlstjs753@gmail.com>
2025-03-13 14:41:11 +0900

    03-13 #level4 코드 리팩토링

--- src/level4/Kiosk.java
+++ src/level4/Kiosk.java
@@ -14,7 +14,7 @@ public class Kiosk {
   List<Menu> menus;
 
   // 리스트를 받게 합니다.
-  Kiosk(List<Menu> menus){
+  Kiosk(List<Menu> menus) {
     this.menus = menus;
   }
 
@@ -22,22 +22,22 @@ public class Kiosk {
     Scanner sc = new Scanner(System.in);
     int orderCategory;
     Menu menu;
-    while (true){
+    while (true) {
       // 리스트와 Menu 클래스 활용하여, 상위 카테고리 메뉴를 출력해야하는 겁니다.
       Output.printOutput("[ Burger King MAIN MENU ]");
-      for (int i=1; i <= menus.size(); i++){
-        System.out.printf("%d. %s\n", i, menus.get(i-1).getCategoryName());
+      for (int i = 1; i <= menus.size(); i++) {
+        System.out.printf("%d. %s\n", i, menus.get(i - 1).getCategoryName());
       }
       Output.printOutput("0. 종료");
 
       // 값을 받아야한다.
       try {
         orderCategory = sc.nextInt();
-        if(orderCategory==0){
+        if (orderCategory == 0) {
           break;
         }
-        menu = menus.get(orderCategory-1);
-      } catch (InputMismatchException e){
+        menu = menus.get(orderCategory - 1);
+      } catch (InputMismatchException e) {
         Output.printOutput("잘못입력했습니다.");
         sc.nextLine();
         continue;
@@ -48,26 +48,26 @@ public class Kiosk {
       }
 
 
-      Output.printOutput("[ "+menu.getCategoryName()+" ]");
+      Output.printOutput("[ " + menu.getCategoryName() + " ]");
       menu.viewMenuItems();
       Output.printOutput("0. 뒤로가기.");
       int subExit = 1;
-      while(subExit !=0){
+      while (subExit != 0) {
         // int 값 받고 !
         int orderMenuItem;
         try {
           // 이상 없을 경우 출력
           orderMenuItem = sc.nextInt();
-          if(orderMenuItem == 0){
+          if (orderMenuItem == 0) {
             Output.printLineDivider();
             Output.printOutput("메인 메뉴로 이동합니다.");
           } else {
             // 번호를 받으면 menu.items Get
             List<MenuItem> itemList = menu.getMenuItems();
-            System.out.printf("선택한 메뉴: %s | %s | %s \n",itemList.get(orderMenuItem-1).menuName,itemList.get(orderMenuItem-1).menuPrice,itemList.get(orderMenuItem-1).menuDescription);
+            System.out.printf("선택한 메뉴: %s | %s | %s \n", itemList.get(orderMenuItem - 1).menuName, itemList.get(orderMenuItem - 1).menuPrice, itemList.get(orderMenuItem - 1).menuDescription);
           }
           break;
-        } catch (Exception e){ // 값이 이상할 경우 오류 출력
+        } catch (Exception e) { // 값이 이상할 경우 오류 출력
           System.out.println("값이 이상합니다.");
           sc.nextLine();
         }
--- src/level4/Menu.java
+++ src/level4/Menu.java
@@ -9,24 +9,24 @@ public class Menu {
   List<MenuItem> menuItems = new ArrayList<>();
 
   // 생성자
-  Menu(String categoryName){
+  Menu(String categoryName) {
     this.categoryName = categoryName;
   }
 
   // 리스트를 순차적으로 보여주는 함수
-  public void viewMenuItems(){
-    for (int i=0; i < menuItems.size(); i++){
-      System.out.printf("%-30s | %s원 | %s\n",(i+1)+ ". "+ menuItems.get(i).menuName,menuItems.get(i).menuPrice,menuItems.get(i).menuDescription);
+  public void viewMenuItems() {
+    for (int i = 0; i < menuItems.size(); i++) {
+      System.out.printf("%-30s | %s원 | %s\n", (i + 1) + ". " + menuItems.get(i).menuName, menuItems.get(i).menuPrice, menuItems.get(i).menuDescription);
     }
   }
 
   // List를 리턴하는 함수
-  public List<MenuItem> getMenuItems(){
+  public List<MenuItem> getMenuItems() {
     return this.menuItems;
   }
 
   // 카테고리 이름을 반환
-  public String getCategoryName(){
+  public String getCategoryName() {
     return this.categoryName;
   }
 
