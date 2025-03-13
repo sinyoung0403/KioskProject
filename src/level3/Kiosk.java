9b15186a96e5430263cb9eeb5030c4a98cf49780
sinyoung0403 <tlstjs753@gmail.com>
2025-03-13 14:40:36 +0900

    03-13 #level3 코드 스타일 변경

--- src/level3/Kiosk.java
+++ src/level3/Kiosk.java
@@ -10,39 +10,39 @@ public class Kiosk {
   List<MenuItem> menuItems;
 
   // Kiosk 클래스 생성자
-  Kiosk(List<MenuItem> menuItems){
+  Kiosk(List<MenuItem> menuItems) {
     this.menuItems = menuItems;
   }
 
   // 입력과 반복문 로직
-  public void start(){
+  public void start() {
     int exit = 1;
-    int inputNumber=-1;
+    int inputNumber = -1;
     Scanner sc = new Scanner(System.in);
-    while(exit != 0){
+    while (exit != 0) {
       System.out.println("[BBQ Menu]");
       int i = 1;
-      for (MenuItem a:menuItems){
-        System.out.printf("%s | %s | %s\n",i+". "+a.menuName,a.menuPrice ,a.menuDescription);
+      for (MenuItem a : menuItems) {
+        System.out.printf("%s | %s | %s\n", i + ". " + a.menuName, a.menuPrice, a.menuDescription);
         i++;
       }
-      System.out.printf("%s | %s","0. 종료","0\n");
+      System.out.printf("%s | %s", "0. 종료", "0\n");
       System.out.println("주문을 입력해주세요.");
-      try{
+      try {
         inputNumber = sc.nextInt();
-      } catch (InputMismatchException e){ // 숫자 입력이 아닐 시
+      } catch (InputMismatchException e) { // 숫자 입력이 아닐 시
         System.out.println("숫자를 입력하셔야 합니다.");
         sc.nextLine();
         continue;
       }
 
       // inputNumber 가 0 일 경우 반복문 탈출
-      if(inputNumber == 0) {
+      if (inputNumber == 0) {
         exit = 0;
-      } else{
+      } else {
         // 값이 올바를 경우 출력
         try {
-          System.out.printf("%s | 가격: %d | 설명: %s\n", menuItems.get(inputNumber-1).menuName, menuItems.get(inputNumber-1).menuPrice, menuItems.get(inputNumber-1).menuDescription);
+          System.out.printf("%s | 가격: %d | 설명: %s\n", menuItems.get(inputNumber - 1).menuName, menuItems.get(inputNumber - 1).menuPrice, menuItems.get(inputNumber - 1).menuDescription);
         } catch (Exception e) { // 번호 밖이면, 오류 출력
           System.out.println("번호 내에서 입력 부탁드립니다.");
         }
--- src/level3/Main.java
+++ src/level3/Main.java
@@ -10,11 +10,11 @@ public class Main {
     List<MenuItem> menuItems = new ArrayList<>();
 
     // add 함수를 통해 new MenuItem() List 에 삽입
-    menuItems.add(new MenuItem("Whopper™",7100,"버거킹의 대표 메뉴로, 두툼한 패티와 신선한 채소가 어우러져 클래식한 맛을 제공하는 와퍼"));
-    menuItems.add(new MenuItem("Quattro Cheese Whopper",7900,"네 가지 치즈가 어우러져 진한 치즈 풍미를 느낄 수 있는 와퍼"));
-    menuItems.add(new MenuItem("Monster Whopper",9300,"두툼한 더블 패티와 바삭한 베이컨, 매콤한 소스로 강렬한 맛을 자랑하는 와퍼"));
-    menuItems.add(new MenuItem("Whole Shrimp Whopper",7900,"탱글탱글한 통새우와 신선한 야채가 어우러져 해산물의 풍미를 느낄 수 있는 버거"));
-    menuItems.add(new MenuItem("Whopper Jr",4700,"클래식 와퍼의 맛을 그대로 담은 작은 사이즈의 버거"));
+    menuItems.add(new MenuItem("Whopper™", 7100, "버거킹의 대표 메뉴로, 두툼한 패티와 신선한 채소가 어우러져 클래식한 맛을 제공하는 와퍼"));
+    menuItems.add(new MenuItem("Quattro Cheese Whopper", 7900, "네 가지 치즈가 어우러져 진한 치즈 풍미를 느낄 수 있는 와퍼"));
+    menuItems.add(new MenuItem("Monster Whopper", 9300, "두툼한 더블 패티와 바삭한 베이컨, 매콤한 소스로 강렬한 맛을 자랑하는 와퍼"));
+    menuItems.add(new MenuItem("Whole Shrimp Whopper", 7900, "탱글탱글한 통새우와 신선한 야채가 어우러져 해산물의 풍미를 느낄 수 있는 버거"));
+    menuItems.add(new MenuItem("Whopper Jr", 4700, "클래식 와퍼의 맛을 그대로 담은 작은 사이즈의 버거"));
 
     Kiosk kiosk = new Kiosk(menuItems);
     kiosk.start();
